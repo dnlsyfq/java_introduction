f52aac41b33408f91a06bbb6ead9b89b3e530e90
dnlsyfq <danialsyafiq@outlook.my>
2024-09-21 00:17:18 +0800

    'update'

--- src/Demo1.java
+++ src/Demo1.java
@@ -1,4 +1,8 @@
 
-public class Demo1 {
-
+class Demo1 {
+	public static void main(String[] args) {
+	    System.out.println("Welcome");
+	    System.out.println("This is a demo"); 
+	    System.out.println("Bye");
+	}
 }
