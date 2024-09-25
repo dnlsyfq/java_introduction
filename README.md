4672af6734c080d9305b1557e3b9a86d8944a58e
dnlsyfq <danialsyafiq@outlook.my>
2024-09-25 23:40:20 +0800

    'update'

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 ```
 System.out.println("Write once, run anywhere!");
 ```
+
+* create string variable
+```
+String <variable>;
+
+
+String name;
+name = 'James';
+```
---
+++ src/DemoString.java
@@ -0,0 +1,11 @@
+
+public class DemoString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name;
+		name = "James";
+		System.out.println(name);
+	}
+
+}
